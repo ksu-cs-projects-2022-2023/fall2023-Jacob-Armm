b5db4ed0ecdd3d19d609fb16cc1aada27785438c
jdlegg <jdlegg@ksu.edu>
2023-11-03 13:49:32 -0500

    Fixed assignment description

--- SlangEmbeddedInterfacingMVP/src/main/scala/demos/assignments/Assignment2a.scala
+++ SlangEmbeddedInterfacingMVP/src/main/scala/demos/assignments/Assignment2a.scala
@@ -2,22 +2,50 @@
 package demos.assignments
 
 import org.sireum._
+import devices._
+import utils.PinModeUtil._
+import architecture.{Config, LPConn}
+import architecture.impl.ArchImpl
+import architecture.impl.builtin.firmata._
 
 object Assignment2a extends App {
   def main(args: ISZ[String]): Z = {
 
-    // Define your logical pins
+    // In this example we will be blinking the on board LED on for 1 second then off for 1 second on repeat
 
-    // Define a pin map from your logical pins to physical pins
+    // 1) Define your logical pin
+    // Example: val pin: Pin = Pin("pinName", PinMode.MODE)
 
-    // Define a config that takes in your pin map, an architecture implementation, and a port
+    // 2) Define a pin map from your logical pins to physical pins
+    /* Example:
 
-    // Initialize the architecture with the configuration and list of logical pins
+          val pinMap: Map[String, Z] = Map.empty[String, Z] ++ ISZ(
+            "pinAlias1" ~> assignOutNumber1,
+            "pinAlias2" ~> assignOutNumber2
+          )
 
-    // Write a state machine to blink the onboard LED (pin 13)
+     */
+
+    // 4) Define a config that takes in your pin map, an architecture implementation, and a port
+    // Example: val config = Config(pinMap, implGetter.getImpl(pinMap), None())
+
+    // 5) Initialize the architecture with the configuration and list of logical pins
+    // Example: LPConn.init(config, ISZ(pin1, pin2, ...))
+
+    // 6) Create an LED object
+    // Example: val device: Device = Device.createDevice(devicePin1, devicePin2, ...)
+
+    // 7) Write a state machine to blink the onboard
 
     return 0
   }
-
-  //Define an extension to the firmata implementation
 }
+
+// 3) Define an extension to the firmata implementation 
+    /* Example:
+
+        @ext object implGetter {
+          def getterMethodName(pinMap: Map[String, Z]): ArchImpl = $
+        }
+
+     */
